7067dc0f30465715cf440fb064a3b572c34cdab3
Serg Shkodin <mycrr25@gmail.com>
2023-05-01 16:33:28 +0300

    DZ-1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,11 +1,11 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	let arr = [];
+	const arr = [];
 
-	let d = b ** b - 4 * a * c;
+	const d = b ** b - 4 * a * c;
 
-	if (d = 0) {
+	if (d === 0) {
 
 		let x = -b / (2 * a);
 		arr.push(x);
@@ -22,4 +22,17 @@ console.log(solveEquation(0, 1, 2))
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
+
+let P = percent / 12 / 100;
+let S = amount - contribution;
+let n = countMonths;
+
+
+let res = S * (P + (P / (((1 + P)**n) - 1))).toFixed(7);
+let num = Number(res);
+let sym = num * countMonths;
+return sym
 }
+
+
+console.log(calculateTotalMortgage(10,0,50000,12))
