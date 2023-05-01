c50a931609eb30f30775291e689fd13d1ada9c07
Serg Shkodin <mycrr25@gmail.com>
2023-05-01 15:04:57 +0300

    DZ-1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+
+	let d = b ** b - 4 * a * c;
+
+	if (d = 0) {
+
+		let x = -b / (2 * a);
+		arr.push(x);
+	} else if (d > 0) {
+		let x = (-b + Math.sqrt(d)) / (2 * a);
+		let y = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x, y)
+	}
+	return arr;
+
 }
 
+console.log(solveEquation(0, 1, 2))
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
