3dfbb7e22a886fa69fe8a57e0486e1a73bb55f0d
Serg Shkodin <mycrr25@gmail.com>
2023-05-03 19:40:23 +0300

    DZ-1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,36 +1,45 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	const arr = [];
+	const roots = [];
 
-	const d = b ** b - 4 * a * c;
+	const discriminant = b ** 2 - 4 * a * c;
 
-	if (d === 0) {
+	if (discriminant === 0) {
 
 		let x = -b / (2 * a);
-		arr.push(x);
-	} else if (d > 0) {
-		let x = (-b + Math.sqrt(d)) / (2 * a);
-		let y = (-b - Math.sqrt(d)) / (2 * a);
-		arr.push(x, y)
+		roots.push(x);
+	} else if (discriminant > 0) {
+		let x = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let y = (-b - Math.sqrt(discriminant)) / (2 * a);
+		roots.push(x, y)
 	}
-	return arr;
+	return roots;
 
 }
 
 console.log(solveEquation(0, 1, 2))
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	Number(percent)
+	Number(contribution)
+	Number(amount)
+	Number(countMonths)
 
+
+	if  (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+		return sym;
+	}
 
 let P = percent / 12 / 100;
 let S = amount - contribution;
 let n = countMonths;
 
 
-let res = S * (P + (P / (((1 + P)**n) - 1))).toFixed(7);
-let num = Number(res);
-let sym = num * countMonths;
+let res = S * (P + (P / (((1 + P)**n) - 1))); //.toFixed(7);
+let sym = res * countMonths;
+sym = sym.toFixed(2);
+isNaN(sym)
 return sym
 }
 
