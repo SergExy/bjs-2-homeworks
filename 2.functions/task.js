57f0d951724a2aeb99ea9bed22f1432564b4f271
Serg Shkodin <mycrr25@gmail.com>
2023-05-14 21:39:10 +0300

    DZ-2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,29 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
 
-  return { min: min, max: max, avg: avg };
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum = arr[i] + sum
+
+  }
+  let avg = sum / arr.length;
+
+  avg = avg.toFixed(2);
+
+  return { min: min, max: max, avg: +avg };
 }
+
+console.log(getArrayParams(-9, 99, -5));
+
+
+
 
 function summElementsWorker(...arr) {
 
@@ -19,6 +41,6 @@ function averageEvenElementsWorker(...arr) {
 
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
 
 }
