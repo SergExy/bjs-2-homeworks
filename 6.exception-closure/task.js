c4a1f56b4624dd241743a656c77e187726b51ea9
Serg Shkodin <mycrr25@gmail.com>
2023-06-12 21:33:45 +0300

    DZ-2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,14 @@
-﻿
+﻿function parseCount(v) {
+    if (isNaN(Number.parseFloat(v))){
+        throw new Error("Невалидное значение");;
+    }
+ return Number.parseFloat(v);
+}
+
+function validateCount(vv) {
+  try {
+    return parseCount(vv);
+  } catch (error) {
+    return error;
+  }
+}
